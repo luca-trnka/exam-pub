fed39e073747da5cfb57bfdf854f534ad06ad593
luca-trnka <lucie-trnkova76@seznam.cz>
2023-10-22 15:22:36 +0200

    user and order entities created

---
+++ src/main/java/com/example/pub/models/Order.java
@@ -0,0 +1,59 @@
+package com.example.pub.models;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
+public class Order {
+    @Id
+    private Long id;
+    private Long productId;
+    private int amount;
+    private double price;
+
+    public Order(Long id, Long productId, int amount, double price) {
+        this.id = id;
+        this.productId = productId;
+        this.amount = amount;
+        this.price = price;
+    }
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
